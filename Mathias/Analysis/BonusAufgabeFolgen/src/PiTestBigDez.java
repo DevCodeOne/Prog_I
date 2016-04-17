dc4176f5bfe03170e20283b8581c030a492da759
Mathias83 <mathias_fedder@gmx.net>
2016-04-17 09:32:20 +0200

    BigDecimal erweitert

--- Mathias/Analysis/BonusAufgabeFolgen/src/PiTestBigDez.java
+++ Mathias/Analysis/BonusAufgabeFolgen/src/PiTestBigDez.java
@@ -6,52 +6,42 @@ public class PiTestBigDez {
 	static BigDecimal zwei = new BigDecimal(2);
 
 	public static void main(String[] args) {
-		/*BigDecimal ak = new BigDecimal(0);
+		BigDecimal ak = new BigDecimal(0);
 		BigDecimal bk = new BigDecimal(0);
 		BigDecimal akminus = new BigDecimal(3);
-		BigDecimal bkminus = new BigDecimal(2.0 * Math.sqrt(akminus.doubleValue()));
+		BigDecimal bkminus = (sqrt(akminus).multiply(zwei));
 
 		double EPSILON = 1E-20;
 
-		do {
-			bk = bkminus.divide(eins);
-			ak = akminus.divide(eins);
-			bk.add(ak);
-			bk.divide(zwei);
-			ak = akminus.multiply(bk);
+		for (int i = 0; i < 100; i++) {
+			bkminus = eins.divide(bkminus, 130, RoundingMode.HALF_UP);
+			ak = eins.divide(akminus, 130, RoundingMode.HALF_UP);
+			bkminus.add(ak);
 
-			akminus = Math.sqrt(akminus * bk);
+			bkminus = zwei.divide(bkminus, 130, RoundingMode.HALF_UP);
+			// ak = akminus.multiply(bk);
 
-			bkminus = bk.pow(1);
+			akminus = sqrt(akminus.multiply(bk));
 
-		} while (ak.scale() < Integer.parseInt(args[0]));*/
-		sqrt(zwei);
-		System.out.println(zwei.toString());
-		
+		}
+
+		System.out.println(akminus.toString());
+		System.out.println(bkminus.toString());
 
 	}
 
 	public static BigDecimal sqrt(BigDecimal a) {
-		System.out.println(a.toString());
 		BigDecimal xn = a;
-		System.out.println(xn.toString());
 		BigDecimal xnp1 = new BigDecimal(0);
-		System.out.println(xnp1.toString());
 		xn.add(eins);
-		System.out.println(xn.toString());
-		xn.divide(zwei, 10, RoundingMode.HALF_UP);
-		System.out.println(xn.toString());
-		for (int i =0;i<100;i++){
-		xnp1 = a.divide(xn, 10, RoundingMode.HALF_UP); 
-		System.out.println(xnp1.toString());
-		xnp1 = xn.add(xnp1);
-		System.out.println(xnp1.toString());
-		xn = xnp1.divide(zwei, 10, RoundingMode.HALF_UP);
-		System.out.println(xn.toString());
-		}	
-		System.out.println(xn.toString());
-		
+		xn.divide(zwei, 130, RoundingMode.HALF_UP);
+		for (int i = 0; i < 99; i++) {
+			System.out.println(i + " " + xn.toString());
+			xnp1 = a.divide(xn, 130, RoundingMode.HALF_UP);
+			xnp1 = xn.add(xnp1);
+			xn = xnp1.divide(zwei, 130, RoundingMode.HALF_UP);
+		}
 		return xn;
-			
+
 	}
 }
