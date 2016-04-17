7624fe35cb9cf6eaba80795cf79dfd802a97d7a8
Mathias83 <mathias_fedder@gmx.net>
2016-04-17 08:09:47 +0200

    BigDecimal erweitert

--- Mathias/Analysis/BonusAufgabeFolgen/src/PiTestBigDez.java
+++ Mathias/Analysis/BonusAufgabeFolgen/src/PiTestBigDez.java
@@ -1,8 +1,9 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class PiTestBigDez {
 	static BigDecimal eins = new BigDecimal(1);
-	static BigDecimal zwei = new BigDecimal(1);
+	static BigDecimal zwei = new BigDecimal(2);
 
 	public static void main(String[] args) {
 		/*BigDecimal ak = new BigDecimal(0);
@@ -25,19 +26,32 @@ public class PiTestBigDez {
 
 		} while (ak.scale() < Integer.parseInt(args[0]));*/
 		sqrt(zwei);
-		zwei.toString();
+		System.out.println(zwei.toString());
 		
 
 	}
 
 	public static BigDecimal sqrt(BigDecimal a) {
-		a.add(eins);
-		a.divide(zwei);
+		System.out.println(a.toString());
+		BigDecimal xn = a;
+		System.out.println(xn.toString());
+		BigDecimal xnp1 = new BigDecimal(0);
+		System.out.println(xnp1.toString());
+		xn.add(eins);
+		System.out.println(xn.toString());
+		xn.divide(zwei, 10, RoundingMode.HALF_UP);
+		System.out.println(xn.toString());
 		for (int i =0;i<100;i++){
-		a.add	
-			
-		}		
-		return a;
+		xnp1 = a.divide(xn, 10, RoundingMode.HALF_UP);
+		System.out.println(xnp1.toString());
+		xnp1 = xn.add(xnp1);
+		System.out.println(xnp1.toString());
+		xn = xnp1.divide(zwei, 10, RoundingMode.HALF_UP);
+		System.out.println(xn.toString());
+		}	
+		System.out.println(xn.toString());
+		
+		return xn;
 			
 	}
 }
